21125cdb973b3a60f8957e727971162747eb13d7
Vladislav <nikolaenko_vladislav@icloud.com>
2022-06-29 11:50:40 +0300

    refactoring code

--- src/Main.java
+++ src/Main.java
@@ -2,79 +2,53 @@ import java.io.*;
 
 public class Main {
 
-    private static final String[] nameDirectories = {"src" , "res" , "savegames", "temp", "main", "test", "drawables", "vectors", "icons"};
+    private static final String[] directories = { "/Users/vladislav/Games/src", "/Users/vladislav/Games/res",
+            "/Users/vladislav/Games/savegames", "/Users/vladislav/Games/temp", "/Users/vladislav/Games/src/main",
+            "/Users/vladislav/Games/src/test", "/Users/vladislav/Games/res/drawables", "/Users/vladislav/Games/res/vectors",
+            "/Users/vladislav/Games/res/icons"};
 
-    private static final String[] nameFiles = {"Main.java", "Utils.java", "temp.txt"};
+    private static final String[] files = {"/Users/vladislav/Games/src/main/Main.java",
+            "/Users/vladislav/Games/src/main/Utils.java", "/Users/vladislav/Games/temp/temp.txt"};
 
     public static void main(String[] args) {
         StringBuilder str = new StringBuilder();
 
-        createDirectories(str, nameDirectories);
-        createFiles(str, nameFiles);
+        createDirectoriesRefactoring(str, directories);
+        createFilesRefactoring(str, files);
+        
         writeReportOfCreateInFileTxt(str);
         readReportOfCreateFromFileTxt();
     }
 
-    private static void createDirectories(StringBuilder str, String[] nameDirectories) {
+    private static void createDirectoriesRefactoring(StringBuilder str, String[] directories) {
         str.append("Создание папок: ");
-        File dir;
-        for (String name : nameDirectories) {
-            switch (name) {
-                case "src", "res", "savegames", "temp" -> {
-                    dir = new File("/Users/vladislav/Games/" + name);
-                    testCreateDir(dir, name, str);
-                }
-
-                case "main", "test" -> {
-                    dir = new File("/Users/vladislav/Games/src/" + name);
-                    testCreateDir(dir, name, str);
-                }
-
-                case "drawables", "vectors", "icons" -> {
-                    dir = new File("/Users/vladislav/Games/res/" + name);
-                    testCreateDir(dir, name, str);
-                }
-            }
-        }
-    }
-
-    private static void testCreateDir(File dir, String name, StringBuilder str) {
-        if (dir.mkdir())
-            str.append("\n" + name + " complete");
-        else
-            str.append("\n" + name + " something wrong");
-    }
-
-    private static void createFiles (StringBuilder str, String[] nameFiles) {
-        str.append("\nСоздание файлов: ");
-        File file;
-        for (String name : nameFiles) {
-            switch (name) {
-                case "Main.java", "Utils.java" -> {
-                    file = new File("/Users/vladislav/Games/src/main/" + name);
-                    testCreateFiles(file, name, str);
-                }
-
-                case "temp.txt" -> {
-                    file = new File("/Users/vladislav/Games/temp/" + name);
-                    testCreateFiles(file, name, str);
-                }
-            }
-        }
-    }
-
-    private static void testCreateFiles(File file, String name, StringBuilder str) {
-        try {
-            if (file.createNewFile())
+        for (String name : directories) {
+            File dir = new File(name);
+            if (dir.mkdir())
                 str.append("\n" + name + " complete");
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+            else
+                str.append("\n" + name + " something wrong");
+        }
+    }
+
+    private static void createFilesRefactoring(StringBuilder str, String[] files) {
+        str.append("\nСоздание файлов: ");
+        for (String name : files) {
+            File file  = new File(name);
+            try {
+                if (file.createNewFile())
+                    str.append("\n" + name + " complete");
+                else
+                    str.append("\n" + name + " something wrong");
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
         }
     }
 
     private static void writeReportOfCreateInFileTxt(StringBuilder str) {
         System.out.println("Отправка отчета в 'temp.txt'...");
-        try (FileWriter writer = new FileWriter("/Users/vladislav/Games/temp//temp.txt", false)) {
+        try (FileWriter writer = new FileWriter("/Users/vladislav/Games/temp/temp.txt", false)) {
             writer.write(String.valueOf(str));
             writer.flush();
         } catch (IOException ex) {
